cac5d15e458ce1d43ca717cdda44bdbfc105b457
Tomas Frohawk <tom.frohawk@hotmail.co.uk>
2017-12-04 18:03:24 +0000

    Added numbers[] array and for loop

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Just made a small change...");
-
+		int[] numbers;
+		numbers = new int[5];
+		
+		Scanner input = new Scanner(System.in);
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Enter lottery number: ");
+			numbers[i] = input.nextInt();
+		}
+		
+		
+		input.close();
+		System.out.println("Program is terminating. Thank you!");
 	}
 
 }
