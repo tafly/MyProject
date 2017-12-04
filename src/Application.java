013f04525793969c1e9f117037969290a5da3a31
Tomas Frohawk <tom.frohawk@hotmail.co.uk>
2017-12-04 18:10:07 +0000

    added max() class for finding the max number in numbers[] array

--- src/Application.java
+++ src/Application.java
@@ -3,9 +3,25 @@ import java.util.Scanner;
 
 public class Application {
 
+	static void max(int[] numbers) {
+		// create max variable which will store highest number
+		int max = 0;
+		
+		// loop through the values in numbers[] array
+		for (int i = 0; i < 5; i++) {
+			if (numbers[i] > max) max = numbers[i];
+		}
+		
+		// print out the highest number
+		System.out.printf("Max number is %d \n", max);
+	}
+	
+	
 	public static void main(String[] args) {
+		// create array numbers
 		int[] numbers;
 		numbers = new int[5];
+		
 		
 		Scanner input = new Scanner(System.in);
 		
@@ -14,8 +30,10 @@ public class Application {
 			numbers[i] = input.nextInt();
 		}
 		
-		
 		input.close();
+		
+		max(numbers);
+		
 		System.out.println("Program is terminating. Thank you!");
 	}
 
